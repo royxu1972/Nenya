3eada2e4c36a9b4aa6eb406860d9f853c064c2a0
huayao <hywu@outlook.com>
2015-07-28 17:33:46 +0800

    new exp

--- exp/src/RunExp.java
+++ exp/src/RunExp.java
@@ -15,6 +15,8 @@ public class RunExp {
         Simulation s = new Simulation();
         //s.initSubjects(1000);
         //System.out.println(s.getDataItem(6, s.order_1, 30).toString());
-        s.exp1(59,100);
+        //s.exp1(59,100);
+
+        s.exp2cost("alg.txt");
     }
 }
